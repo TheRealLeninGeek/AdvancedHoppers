378c998d5f62867754052db545aaedd4e15a25d6
TheRealLeninGeek <leningeek@gmail.com>
2014-10-19 17:53:40 +0400

    Ooops.

--- src/main/java/com/leningeek/advancedhoppers/AdvancedHoppers.java
+++ src/main/java/com/leningeek/advancedhoppers/AdvancedHoppers.java
@@ -2,6 +2,8 @@ package com.leningeek.advancedhoppers;
 
 import com.leningeek.advancedhoppers.proxy.CommonProxy;
 import com.leningeek.advancedhoppers.references.References;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
