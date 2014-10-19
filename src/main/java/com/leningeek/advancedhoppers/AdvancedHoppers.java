84f94d8ed6a5716b08c7e867d29cfdc0ad57f005
TheRealLeninGeek <leningeek@gmail.com>
2014-10-19 17:06:08 +0400

    Added references. Added a proxy instance to the mod class.

--- src/main/java/com/leningeek/advancedhoppers/AdvancedHoppers.java
+++ src/main/java/com/leningeek/advancedhoppers/AdvancedHoppers.java
@@ -1,9 +1,13 @@
 package com.leningeek.advancedhoppers;
 
+import com.leningeek.advancedhoppers.proxy.CommonProxy;
+import com.leningeek.advancedhoppers.references.References;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.relauncher.SideOnly;
 
 /**
  * Created by Кир on 19.10.2014.
@@ -12,6 +16,9 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 @Mod(name = "Advanced Hoppers", modid = "advancedhoppers", version = "1.7.10-a0.1", canBeDeactivated = true)
 public class AdvancedHoppers {
     public static AdvancedHoppers modInstance = new AdvancedHoppers();
+
+    @SidedProxy(clientSide = References.CLIENT_PROXY_CLASS, serverSide = References.SERVER_PROXY_CLASS)
+    public static CommonProxy proxy;
 
     @Mod.EventHandler
     void preInit(FMLPreInitializationEvent event) {
---
+++ src/main/java/com/leningeek/advancedhoppers/references/References.java
@@ -0,0 +1,16 @@
+package com.leningeek.advancedhoppers.references;
+
+/**
+ * Created by Кир on 19.10.2014.
+ */
+public class References {
+    public static final String MODID = "advancedhoppers";
+
+    public static final String VERSION = "1.7.10-a0.1";
+
+    public static final String NAME = "Advanced Hoppers";
+
+    public static final String CLIENT_PROXY_CLASS = "com.leningeek.advancedhoppers.proxy.ClientProxy";
+
+    public static final String SERVER_PROXY_CLASS = "com.leningeek.advancedhoppers.proxy.ServerProxy";
+}
