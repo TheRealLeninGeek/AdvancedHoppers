0f7e6912012ddb23449f6b2ca68a679fcf2aec60
TheRealLeninGeek <leningeek@gmail.com>
2014-10-19 15:54:57 +0400

    Added a mod class.

---
+++ src/main/java/AdvancedHoppers.java
@@ -0,0 +1,28 @@
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by Кир on 19.10.2014.
+ */
+
+@Mod(name = "Advanced Hoppers", modid = "advancedhoppers", version = "1.7.10-a0.1", canBeDeactivated = true)
+public class AdvancedHoppers {
+    public static AdvancedHoppers modInstance = new AdvancedHoppers();
+
+    @Mod.EventHandler
+    void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
