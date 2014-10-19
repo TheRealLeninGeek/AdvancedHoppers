48f80d95f3cf6fa667ebf02b973bcafafb63ec87
TheRealLeninGeek <leningeek@gmail.com>
2014-10-19 16:12:27 +0400

    Added a creative tab.

---
+++ src/main/java/creativetabs/CreativeTabHoppers.java
@@ -0,0 +1,17 @@
+package creativetabs;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+
+/**
+ * Created by LeninGeek on 19.10.2014.
+ */
+public class CreativeTabHoppers {
+    public static final CreativeTabs TAB = (new CreativeTabs("advancedhoppers") {
+        @Override
+        public Item getTabIconItem() {
+            return Item.getItemFromBlock(Blocks.hopper);
+        }
+    });
+}
